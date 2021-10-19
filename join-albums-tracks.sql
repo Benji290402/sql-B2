d235e806ec0c15bbf70e41818687a116aedcb0fa
benji290402 <benjamin.vialsimon@ynov.com>
2021-10-19 12:06:26 +0200

    Update join-albums-tracks.sql

--- join-albums-tracks.sql
+++ join-albums-tracks.sql
@@ -1,4 +1,4 @@
-SELECT Title AS AlbumName, Name AS TrakName, Milliseconds
+SELECT Title AS AlbumName, Name AS TrackName, Milliseconds
 FROM albums
 INNER JOIN tracks ON albums.AlbumId = tracks.AlbumId
 ORDER BY Milliseconds ASC
