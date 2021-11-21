7c324bbcd445adc97e6b38bb20bd8a1b4ac7e4b5
benji290402 <benjamin.vialsimon@ynov.com>
2021-11-21 19:42:45 +0100

    Update select-invoice-items.sql

--- select-invoice-items.sql
+++ select-invoice-items.sql
@@ -1,5 +1,5 @@
 select distinct invoice_items.InvoiceId, tracks.Name AS InvoiceItem, invoice_items.UnitPrice
 from invoice_items
 inner join tracks on invoice_items.TrackId = tracks.TrackId
-WHERE invoice_items.InvoiceId = "10"
+WHERE invoice_items.InvoiceId = '10'
 ORDER BY Name ASC
